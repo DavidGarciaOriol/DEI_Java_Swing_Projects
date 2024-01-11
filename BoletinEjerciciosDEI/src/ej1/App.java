f9150da0b247896b448ee246c39f8271f72a426b
ciclot <ciclot@iesmartinezm.es>
2024-01-11 21:07:48 +0100

    Primer ejercicio boletín completo

---
+++ BoletinEjerciciosDEI/src/ej1/App.java
@@ -0,0 +1,9 @@
+package ej1;
+
+public class App {
+
+    public static void main(String[] args) {
+        VentanaPrincipal ventanaPrincipal = new VentanaPrincipal();
+        ventanaPrincipal.setVisible(true);
+    }
+}
