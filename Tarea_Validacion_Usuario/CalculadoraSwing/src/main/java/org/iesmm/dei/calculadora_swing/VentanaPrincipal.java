8b871beb69f780b7b0b799759e9c429c1b9b9f76
ciclot <ciclot@iesmartinezm.es>
2024-01-29 21:59:10 +0100

    Terminada calculadora, a falta de scrollbar

--- Tarea_Validacion_Usuario/CalculadoraSwing/src/main/java/org/iesmm/dei/calculadora_swing/VentanaPrincipal.java
+++ Tarea_Validacion_Usuario/CalculadoraSwing/src/main/java/org/iesmm/dei/calculadora_swing/VentanaPrincipal.java
@@ -6,13 +6,21 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class VentanaPrincipal extends JFrame implements ActionListener{
+
+    // COMPONENTES ESTÁTICOS
+    private JFrame frame = this;
     private JPanel panel = new JPanel(new GridLayout(6,4));
     private JTextPane display;
-    private int numero1 = 0;
-    private int numero2 = 0;
-    private int resultado = 0;
+
+    // VARIABLES DE LA LÓGICA DEL PROGRAMA
+    private double numero1 = 0;
+    private double numero2 = 0;
+    private double resultado = 0;
     private char operacion = 'N';
+    private boolean resultado_otenido = false;
     public VentanaPrincipal(){
+
+        // MAQUETACIÓN DEL FRAME PRINCIPAL
 
         super("Calculadora");
         JFrame frame = this;
@@ -21,11 +29,10 @@ public class VentanaPrincipal extends JFrame implements ActionListener{
         setResizable(false);
         setContentPane(panel);
 
-        // DECLARACIÓN DE BOTONES
+        // DECLARACIÓN DE BOTONES DINÁMICOS
 
         for (int i = 0; i < 20; i++){
             JButton button = new JButton();
-
             switch (i){
                 case 0:
                     button.setText("1");
@@ -111,110 +118,191 @@ public class VentanaPrincipal extends JFrame implements ActionListener{
             button.addActionListener(this);
             add(button);
         }
+
+        // CONFECCIÓN DEL DISPLAY
+
         display = new JTextPane();
         display.setText("0");
         display.setEnabled(false);
         display.setDisabledTextColor(Color.black);
         display.setPreferredSize(new Dimension(80,20));
 
+        display.add(new JScrollPane());
+
         panel.add(display);
         pack();
     }
+
+    // LISTENER DE BOTONES PULSADOS
     @Override
     public void actionPerformed(ActionEvent e) {
         String actionCommand = e.getActionCommand();
 
         switch (actionCommand){
+
+            /** Al pulsar los números, se comprueba el display y se agregan si
+             * procede al mismo, modificando la variable correspondiente en función
+             * del contexto y situación de la operación.
+             */
             case "1", "2", "3", "4", "5", "6", "7", "8", "9":
-                if (display.getText().equals("0") || display.getText().equals("#ERROR /0"))
+                if (display.getText().equals("0") || display.getText().equals("#ERROR /0")){
                     display.setText(actionCommand);
-                else display.setText(display.getText() + actionCommand);
+                } else {
+                    display.setText(display.getText() + actionCommand);
+                }
+                if (operacion != 'N'){
+                    if (resultado_otenido){
+                        numero1 = Double.parseDouble(display.getText());
+                    } else {
+                        numero2 = Double.parseDouble(display.getText());
+                    }
+                } else {
+                    numero1 = Double.parseDouble(display.getText());
+                }
                 break;
+
             case "0":
-                if (!display.getText().equals("0"))
-                    if (!display.getText().equals("#ERROR /0"))
+                if (!display.getText().equals("0")){
+                    if (!display.getText().equals("#ERROR /0")){
                         display.setText(display.getText() + actionCommand);
+                        if (operacion != 'N'){
+                            if (resultado_otenido){
+                                numero1 =Double.parseDouble(display.getText());
+                            } else {
+                                numero2 = Double.parseDouble(display.getText());
+                            }
+                        } else {
+                            numero1 = Double.parseDouble(display.getText());
+                        }
+                    }
+                }
                 break;
+
+            /** Agrega un punto para operar con decimales
+             * siempre y cuando no haya uno ya.
+             */
+            case ",":
+                if (!display.getText().contains(".")){
+                    display.setText(display.getText() + ".");
+                }
+                break;
+
+            /** El siguiente bloque controla el estado de la operación,
+             * y a continuación indica el tipo de operación  que se deberá
+             * realizar cuando se pulse en el botón igual (=).
+             */
             case "+":
-                if (!display.getText().equals("#ERROR /0"))
-                    numero1 = numero1!=0?numero1:Integer.parseInt(display.getText());
+                if (!display.getText().equals("#ERROR /0")){
+                    if (operacion != 'N') numero2 = 0;
+                    resultado_otenido = false;
+                    operacion = '+';
+                }
                 display.setText("0");
-                operacion = '+';
                 break;
+
             case "-":
-                if (!display.getText().equals("#ERROR /0"))
-                    numero1 = numero1!=0?numero1:Integer.parseInt(display.getText());
+                if (!display.getText().equals("#ERROR /0")){
+                    if (operacion != 'N') numero2 = 0;
+                    resultado_otenido = false;
+                    operacion = '-';
+                }
                 display.setText("0");
-                operacion = '-';
                 break;
+
             case "x":
-                if (!display.getText().equals("#ERROR /0"))
-                    numero1 = numero1!=0?numero1:Integer.parseInt(display.getText());
+                if (!display.getText().equals("#ERROR /0")){
+                    if (operacion != 'N') numero2 = 0;
+                    resultado_otenido = false;
+                    operacion = 'x';
+                }
                 display.setText("0");
-                operacion = 'x';
                 break;
+
             case "/":
-                if (!display.getText().equals("#ERROR /0"))
-                    numero1 = numero1!=0?numero1:Integer.parseInt(display.getText());
+                if (!display.getText().equals("#ERROR /0")){
+                    if (operacion != 'N') numero2 = 0;
+                    resultado_otenido = false;
+                    operacion = '/';
+                }
                 display.setText("0");
-                operacion = '/';
                 break;
+
+            /** Realiza la operación pertinente, controlando errores. */
             case "=":
                 if (!display.getText().equals("#ERROR /0")){
-                    try {
-                        if (numero2 == 0)
-                            numero2 = Integer.parseInt(display.getText());
+
                         resultado = operar(numero1, numero2);
                         display.setText(String.valueOf(resultado));
                         numero1 = resultado;
-                        resultado = 0;
+                        resultado_otenido = true;
 
-                    } catch (ArithmeticException ex){
-                        display.setText("#ERROR /0");
-                        numero1 = 0;
-                        numero2 = 0;
-                        resultado = 0;
-                    }
-
+                        if (resultado == Double.POSITIVE_INFINITY || resultado == Double.NEGATIVE_INFINITY){
+                            display.setText("#ERROR /0");
+                            numero1 = 0;
+                            numero2 = 0;
+                            resultado = 0;
+                            resultado_otenido = false;
+                            JOptionPane.showMessageDialog(frame, "ERROR AL INTENTAR DIVIDIR ENTRE CERO");
+                        }
                 }
                 break;
+
+            /** Resetea el display en el contexto de la operación actual. */
             case "CE":
-                // TODO: CLEAR ENTRY
                 display.setText("0");
-                numero2 = 0;
+                if (resultado_otenido){
+                    numero1 = 0;
+                } else {
+                    numero2 = 0;
+                }
                 break;
+
+            /** Reinicia toda la operación, dejando los valores a cero. */
             case "C":
                 display.setText("0");
                 numero1 = 0;
                 numero2 = 0;
                 resultado = 0;
                 operacion = 'N';
+                resultado_otenido = false;
                 break;
+
+            /** Elimina el último número o carácter agregado al display. */
             case "DEL":
                 if (!display.getText().equals("#ERROR /0")){
                     display.setText(eliminarUltimoCaracter(display.getText()));
-                    numero1 = operacion=='N'?Integer.parseInt(display.getText()):numero1;
-                    numero2 = operacion=='N'?numero2:Integer.parseInt(display.getText());
+
+                    if (operacion != 'N'){
+                        if (resultado_otenido){
+                            numero1 = Double.parseDouble(display.getText());
+                        } else {
+                            numero2 = Double.parseDouble(display.getText());
+                        }
+                    } else {
+                        numero1 = Double.parseDouble(display.getText());
+                    }
                 }
                 break;
+
+            /** Funnción para DEBUG en consola. */
             case "SHELL":
                 try {
-                    System.out.println("NUM1: " + (numero1==Integer.parseInt(display.getText())?numero1:Integer.parseInt(display.getText()))
-                            + " | NUM2: " + (numero2==0?Integer.parseInt(display.getText()):numero2)
-                            + " | RESULTADO: " +operar(numero1, (numero2==0?Integer.parseInt(display.getText()):numero2))
+                    System.out.println("NUM1: " + numero1
+                            + " | NUM2: " + numero2
+                            + " | RESULTADO: " + operar(numero1, numero2)
                             + " | OPERADOR: " + operacion);
                 } catch (ArithmeticException ex){
                     System.err.println("ERROR CONTROLADO: DIVIDIR ENTRE 0 - " + ex.getMessage()
-                            + "\nNUM1: " + (numero1==Integer.parseInt(display.getText())?numero1:Integer.parseInt(display.getText()))
-                            + " | NUM2: " + (numero2==0?Integer.parseInt(display.getText()):numero2)
+                            + "\nNUM1: " + numero1
+                            + " | NUM2: " + numero2
                             + " | OPERADOR: " + operacion);
                 }
         }
     }
 
     // CALCULAR EN FUNCIÓN AL OPERADOR PULSADO MÁS RECIENTE
-    private int operar(int num1, int num2){
-        int out = 0;
+    private double operar(double num1, double num2){
+        double out = 0;
         switch (operacion){
             case '+':
                 out = num1 + num2;
@@ -229,11 +317,12 @@ public class VentanaPrincipal extends JFrame implements ActionListener{
                 out = num1 / num2;
                 break;
             default:
-                out = Integer.parseInt(display.getText());
+                out = Double.parseDouble(display.getText());
         }
         return out;
     }
 
+    // ELIMINA EL ÚLTIMO NÚMERO/CARACTER DE LA CADENA DEL DISPLAY
     public static String eliminarUltimoCaracter(String msg) {
         return (msg == null || msg.isEmpty() || msg.length() == 1)
                 ? "0"
